5c7683c8aa694a7421b9464a321e5e71a83c4b3b
AlephNeil <neil.dot.fitzgerald@gmail.com>
2018-02-23 17:35:31 +0000

    blah

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('Hello world!');
+const myname = "Neil"
+const blah = `alpha ${myname} beta`
+console.log(`The value of blah is ${blah}`)
+
+var webdriver = require('selenium-webdriver');
+var browser = new webdriver.Builder()
+    .usingServer()
+    .withCapabilities({'browserName': 'chrome'})
+    .build();
+
+browser.get('http://en.wikipedia.org/wiki/Wiki');
+browser.findElements(webdriver.By.css('[href^="/wiki/"]'))
+    .then(function(links) {
+    console.log('Found', links.length, 'Wiki links.' )
+    browser.quit();
+});
